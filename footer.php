191cd098016d513356615e0c17119b4fa7d551d2
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2024-12-13 02:51:11 +0200

    ff

--- footer.php
+++ footer.php
@@ -1,3 +1,30 @@
+<footer>
+    <div class="footerWrapper wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="" class="footerLogo">
+        <div class="footerColumn">
+            <p>КАТАЛОГ</p>
+            <a href="">Агродрони</a>
+            <a href="">Вузли змішування</a>
+            <a href="">Генератори</a>
+            <a href="">Комплектуючі</a>
+        </div>
+        <div class="footerColumn">
+            <p>СТОРІНКИ</p>
+            <a href="">Каталог</a>
+            <a href="">Внесення ЗЗР</a>
+            <a href="">Академія пілотів</a>
+            <a href="">Блог</a>
+        </div>
+        <div class="footerColumn">
+            <p>Інформація</p>
+            <a href="">lunares.agro@gmail.com</a>
+            <a href="">+380939694642</a>
+
+        </div>
+
+    </div>
+</footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- page-fertilization.php
+++ page-fertilization.php
@@ -1,1 +1,13 @@
 /* Placeholder content for page-fertilization.php */
+
+<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+        <!-- Верхняя дуга -->
+        <path d="M 60 40 c 22 0 40 -18 40 -40 h -40 Z" fill="black" />
+        <!-- Правая дуга -->
+        <!-- Правая дуга -->
+        <path d="M 160 40 c 22 0 40 -18 40 -40 v 40 Z" fill="white" />
+        <!-- Нижняя дуга -->
+        <path d="M 160 160 c 0 22 18 40 40 40 h -40 Z" fill="black" />
+        <!-- Левая дуга -->
+        <path d="M 40 160 c -22 0 -40 18 -40 40 v -40 Z" fill="black" />
+    </svg>
