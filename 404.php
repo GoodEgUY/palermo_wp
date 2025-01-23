2760b3969a73046ff6c234fc72ee5850d8c0d49b
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2025-01-23 23:21:26 +0200

    gg

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+get_header(); // Подключаем header
+/*
+*/
+?>
+<div class="mainWrapper">
+    <section>
+        <div class="policyWrapper wrapper">
+            <h1>404</h1>
+        </div>
+    </section>
+</div>
+<?php
+get_footer(); // Подключаем footer
+?>
